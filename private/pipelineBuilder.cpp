5fdba151ac920b656a63d325109b418544d8745d
Robert Flitney <robert.ogflitney@gmail.com>
2024-01-31 23:27:10 +0000

    Adds IPipelineBuilder

--- private/pipelineBuilder.cpp
+++ private/pipelineBuilder.cpp
@@ -1,39 +1,111 @@
 #include <pipelineBuilder.hpp>
+#include <resourceScope.hpp>
 #include <fstream>
 
 namespace ignis {
 
 PipelineLayoutBuilder::PipelineLayoutBuilder(
     vk::Device device
-) : device(device)
+) : m_device(device)
 {}
 
 PipelineLayoutBuilder& PipelineLayoutBuilder::addSet(
     vk::DescriptorSetLayout setLayout
 ) {
-    setLayouts.push_back(setLayout);
+    m_setLayouts.push_back(setLayout);
     return *this;
 }
 
 PipelineLayoutBuilder& PipelineLayoutBuilder::addPushConstantRange(
     vk::PushConstantRange pcRange
 ) {
-    pushConstantRanges.push_back(pcRange);
+    m_pushConstantRanges.push_back(pcRange);
     return *this;
 }
 
 vk::PipelineLayout PipelineLayoutBuilder::build() {
-    return device.createPipelineLayout(vk::PipelineLayoutCreateInfo {}
-        .setSetLayouts(setLayouts)
-        .setPushConstantRanges(pushConstantRanges)
+    return m_device.createPipelineLayout(vk::PipelineLayoutCreateInfo {}
+        .setSetLayouts(m_setLayouts)
+        .setPushConstantRanges(m_pushConstantRanges)
         );
+}
+
+IPipelineBuilder::IPipelineBuilder(
+    vk::Device device
+) : m_device(device)
+{}
+
+IPipelineBuilder::IPipelineBuilder(
+    vk::Device device,
+    vk::PipelineLayout layout
+) : m_device(device),
+    m_layout(layout)
+{}
+
+vk::ShaderModule IPipelineBuilder::loadShaderModule(const char* filename) {
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+    
+    if (!file.is_open())
+        throw std::runtime_error("Failed to open shader code file: " + std::string { filename });
+    
+    size_t filesize = file.tellg();
+    std::vector<char> code(filesize);
+    file.seekg(0);
+    file.read(code.data(), filesize);
+
+    return m_device.createShaderModule(vk::ShaderModuleCreateInfo {}
+        .setCodeSize(code.size())
+        .setPCode(reinterpret_cast<uint32_t*>(code.data()))
+        );
+}
+
+ComputePipelineBuilder::ComputePipelineBuilder(
+    vk::Device device
+) : IPipelineBuilder(device)
+{}
+
+ComputePipelineBuilder::ComputePipelineBuilder(
+    vk::Device device,
+    vk::PipelineLayout layout
+) : IPipelineBuilder(device, layout)
+{}
+
+ComputePipelineBuilder& ComputePipelineBuilder::setPipelineLayout(vk::PipelineLayout pipelineLayout) {
+    return *this;
+}
+
+ComputePipelineBuilder& ComputePipelineBuilder::setShaderModule(vk::ShaderModule shaderModule) {
+    return *this;
+}
+
+ComputePipelineBuilder& ComputePipelineBuilder::setFunctionName(const char* functionName) {
+    return *this;
+}
+
+vk::ResultValue<PipelineData> ComputePipelineBuilder::build() {
+    PipelineData data;
+
+    auto pipline_result = m_device.createComputePipeline(nullptr, vk::ComputePipelineCreateInfo {}
+        .setLayout(m_layout)
+        .setStage(vk::PipelineShaderStageCreateInfo {}
+            .setModule(m_shaderModule)
+            .setPName(m_functionName.c_str())
+            .setStage(vk::ShaderStageFlagBits::eCompute)));
+
+    return vk::ResultValue<PipelineData> { vk::Result::eSuccess, data };
+}
+
+GraphicsPipelineBuilder::GraphicsPipelineBuilder(
+    vk::Device device
+) : IPipelineBuilder(device)
+{
+    useDefaults();
 }
 
 GraphicsPipelineBuilder::GraphicsPipelineBuilder(
     vk::Device device,
     vk::PipelineLayout pipelineLayout
-) : device(device),
-    pipelineLayout(pipelineLayout)
+) : IPipelineBuilder(device, pipelineLayout)
 {
     useDefaults();
 }
@@ -56,84 +128,84 @@ vk::PipelineColorBlendAttachmentState GraphicsPipelineBuilder::defaultAttachment
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::useDefaults() {
-    dynamicStates = {};
-    dynamicStates.push_back(vk::DynamicState::eViewport);
-    dynamicState = vk::PipelineDynamicStateCreateInfo {};
+    m_dynamicStates = {};
+    m_dynamicStates.push_back(vk::DynamicState::eViewport);
+    m_dynamicState = vk::PipelineDynamicStateCreateInfo {};
     
-    vertexInputState = vk::PipelineVertexInputStateCreateInfo {};
+    m_vertexInputState = vk::PipelineVertexInputStateCreateInfo {};
     
     inputAssemblyState = vk::PipelineInputAssemblyStateCreateInfo {}
         .setTopology(vk::PrimitiveTopology::eTriangleList)
         ;
     
-    depthStencilState = std::nullopt;
-    tessellationState = std::nullopt;
+    m_depthStencilState = std::nullopt;
+    m_tessellationState = std::nullopt;
 
-    rasterizationState = vk::PipelineRasterizationStateCreateInfo {}
+    m_rasterizationState = vk::PipelineRasterizationStateCreateInfo {}
         .setCullMode(vk::CullModeFlagBits::eBack)
         .setFrontFace(vk::FrontFace::eClockwise)
         .setPolygonMode(vk::PolygonMode::eFill)
         .setLineWidth(1.f)
         ;
     
-    multisampleState = vk::PipelineMultisampleStateCreateInfo {}
+    m_multisampleState = vk::PipelineMultisampleStateCreateInfo {}
         .setRasterizationSamples(vk::SampleCountFlagBits::e1)
         ;
     
-    attachmentBlendStates = {};
-    colorBlendState = vk::PipelineColorBlendStateCreateInfo {};
+    m_attachmentBlendStates = {};
+    m_colorBlendState = vk::PipelineColorBlendStateCreateInfo {};
 
-    viewports = {};
-    scissors = {};
-    viewportState = vk::PipelineViewportStateCreateInfo {};
+    m_viewports = {};
+    m_scissors = {};
+    m_viewportState = vk::PipelineViewportStateCreateInfo {};
 
-    colorAttachmentFormats = {};
-    renderingCreateInfo = vk::PipelineRenderingCreateInfoKHR {};
+    m_colorAttachmentFormats = {};
+    m_renderingCreateInfo = vk::PipelineRenderingCreateInfoKHR {};
     
     return *this;
 }
 
-vk::ResultValue<GraphicsPipelineBuilder::PipelineData> GraphicsPipelineBuilder::build() {
-    dynamicState
-        .setDynamicStates(dynamicStates)
+vk::ResultValue<PipelineData> GraphicsPipelineBuilder::build() {
+    m_dynamicState
+        .setDynamicStates(m_dynamicStates)
         ;
 
-    viewportState
-        .setViewports(viewports)
-        .setScissors(scissors)
+    m_viewportState
+        .setViewports(m_viewports)
+        .setScissors(m_scissors)
         ;
     
-    vertexInputState
-        .setVertexAttributeDescriptions(vertexAttributes)
-        .setVertexBindingDescriptions(vertexBindings)
+    m_vertexInputState
+        .setVertexAttributeDescriptions(m_vertexAttributes)
+        .setVertexBindingDescriptions(m_vertexBindings)
         ;
 
-    colorBlendState
-        .setAttachments(attachmentBlendStates)
+    m_colorBlendState
+        .setAttachments(m_attachmentBlendStates)
         ;
     
-    renderingCreateInfo
-        .setColorAttachmentFormats(colorAttachmentFormats)
+    m_renderingCreateInfo
+        .setColorAttachmentFormats(m_colorAttachmentFormats)
         ;
 
     auto createInfo = vk::GraphicsPipelineCreateInfo {}
-        .setLayout(pipelineLayout)
-        .setStages(stages)
-        .setPDynamicState(&dynamicState)
-        .setPVertexInputState(&vertexInputState)
+        .setLayout(m_layout)
+        .setStages(m_stages)
+        .setPDynamicState(&m_dynamicState)
+        .setPVertexInputState(&m_vertexInputState)
         .setPInputAssemblyState(&inputAssemblyState)
-        .setPDepthStencilState(depthStencilState ? &*depthStencilState : nullptr)
-        .setPTessellationState(tessellationState ? &*tessellationState : nullptr)
-        .setPRasterizationState(&rasterizationState)
-        .setPMultisampleState(&multisampleState)
-        .setPColorBlendState(&colorBlendState)
-        .setPViewportState(&viewportState)
-        .setPNext(&renderingCreateInfo)
+        .setPDepthStencilState(m_depthStencilState ? &*m_depthStencilState : nullptr)
+        .setPTessellationState(m_tessellationState ? &*m_tessellationState : nullptr)
+        .setPRasterizationState(&m_rasterizationState)
+        .setPMultisampleState(&m_multisampleState)
+        .setPColorBlendState(&m_colorBlendState)
+        .setPViewportState(&m_viewportState)
+        .setPNext(&m_renderingCreateInfo)
         ;
     
     PipelineData ret;
 
-    vk::ResultValue<vk::Pipeline> pipelineResult = device.createGraphicsPipeline(
+    vk::ResultValue<vk::Pipeline> pipelineResult = m_device.createGraphicsPipeline(
         VK_NULL_HANDLE,
         createInfo
     );
@@ -142,34 +214,34 @@ vk::ResultValue<GraphicsPipelineBuilder::PipelineData> GraphicsPipelineBuilder::
         return vk::ResultValue<PipelineData> { pipelineResult.result, ret };
 
     ret.pipeline = pipelineResult.value;
-    ret.shaderModules = modules;
-    ret.pipelineLayout = pipelineLayout;
+    ret.shaderModules = m_modules;
+    ret.pipelineLayout = m_layout;
 
     return vk::ResultValue<PipelineData> { vk::Result::eSuccess, ret };
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::setPipelineLayout(vk::PipelineLayout pl) {
-    pipelineLayout = pl;
+    m_layout = pl;
     return *this;
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::addDynamicState(vk::DynamicState ds) {
-    dynamicStates.push_back(ds);
+    m_dynamicStates.push_back(ds);
     return *this;
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::setVertexInputState(vk::PipelineVertexInputStateCreateInfo vis) {
-    vertexInputState = vis;
+    m_vertexInputState = vis;
     return *this;
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::addVertexAttribute(vk::VertexInputAttributeDescription attribute) {
-    vertexAttributes.push_back(attribute);
+    m_vertexAttributes.push_back(attribute);
     return *this;
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::addVertexBinding(vk::VertexInputBindingDescription binding) {
-    vertexBindings.push_back(binding);
+    m_vertexBindings.push_back(binding);
     return *this;
 }
 
@@ -179,74 +251,57 @@ GraphicsPipelineBuilder& GraphicsPipelineBuilder::setInputAssemblyState(vk::Pipe
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::setDepthStencilState(vk::PipelineDepthStencilStateCreateInfo dss) {
-    depthStencilState = dss;
+    m_depthStencilState = dss;
     return *this;
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::setTessellationState(std::optional<vk::PipelineTessellationStateCreateInfo> ts) {
-    tessellationState = ts;
+    m_tessellationState = ts;
     return *this;
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::setRasterizationState(vk::PipelineRasterizationStateCreateInfo rs) {
-    rasterizationState = rs;
+    m_rasterizationState = rs;
     return *this;
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::addAttachmentBlendState(vk::PipelineColorBlendAttachmentState abs) {
-    attachmentBlendStates.push_back(abs);
+    m_attachmentBlendStates.push_back(abs);
     return *this;
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::addScissor(vk::Rect2D scissor) {
-    scissors.push_back(scissor);
+    m_scissors.push_back(scissor);
     return *this;
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::addViewport(vk::Viewport viewport) {
-    viewports.push_back(viewport);
+    m_viewports.push_back(viewport);
     return *this;
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::addStage(vk::PipelineShaderStageCreateInfo stage) {
-    stages.push_back(stage);
+    m_stages.push_back(stage);
     return *this;
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::addStageFromFile(const char* filename, const char* funcName, vk::ShaderStageFlagBits shaderStage) {
-    modules.push_back(loadShaderModule(filename));
+    m_modules.push_back(loadShaderModule(filename));
 
     return addStage(vk::PipelineShaderStageCreateInfo {}
-        .setModule(modules.back())
+        .setModule(m_modules.back())
         .setPName(funcName)
         .setStage(shaderStage));
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::setRenderingCreateInfo(vk::PipelineRenderingCreateInfoKHR rci) {
-    renderingCreateInfo = rci;
+    m_renderingCreateInfo = rci;
     return *this;
 }
 
 GraphicsPipelineBuilder& GraphicsPipelineBuilder::addColorAttachmentFormat(vk::Format format) {
-    colorAttachmentFormats.push_back(format);
+    m_colorAttachmentFormats.push_back(format);
     return *this;
-}
-
-vk::ShaderModule GraphicsPipelineBuilder::loadShaderModule(const char* filename) {
-    std::ifstream file(filename, std::ios::binary | std::ios::ate);
-    
-    if (!file.is_open())
-        throw std::runtime_error("Failed to open shader code file: " + std::string { filename });
-    
-    size_t filesize = file.tellg();
-    std::vector<char> code(filesize);
-    file.seekg(0);
-    file.read(code.data(), filesize);
-
-    return device.createShaderModule(vk::ShaderModuleCreateInfo {}
-        .setCodeSize(code.size())
-        .setPCode(reinterpret_cast<uint32_t*>(code.data()))
-        );
 }
 
 }
--- public/pipelineBuilder.hpp
+++ public/pipelineBuilder.hpp
@@ -1,50 +1,87 @@
 #pragma once
 
-#include <vulkan/vulkan.hpp>
+#include "libraries.hpp"
+#include "resourceScope.hpp"
 #include <optional>
 
 namespace ignis {
 
 class PipelineLayoutBuilder {
-    vk::Device device;
+    vk::Device m_device;
 
 public:
-    std::vector<vk::DescriptorSetLayout> setLayouts {};
-    std::vector<vk::PushConstantRange> pushConstantRanges {};
+    std::vector<vk::DescriptorSetLayout> m_setLayouts {};
+    std::vector<vk::PushConstantRange> m_pushConstantRanges {};
 
     PipelineLayoutBuilder(vk::Device device);
 
     PipelineLayoutBuilder& addSet(vk::DescriptorSetLayout setLayout);
     PipelineLayoutBuilder& addPushConstantRange(vk::PushConstantRange pcRange);
 
+    vk::ShaderModule loadShaderModule(const char* filename);
+
     vk::PipelineLayout build();
 };
 
-class GraphicsPipelineBuilder {
-    vk::Device device;
+struct PipelineData {
+    std::vector<vk::ShaderModule> shaderModules;
+    vk::Pipeline pipeline;
+    vk::PipelineLayout pipelineLayout;
+};
+
+class IPipelineBuilder {
+protected:
+    vk::Device m_device;
+    vk::PipelineLayout m_layout;
+    std::vector<vk::ShaderModule> m_modules;
 
 public:
-    vk::PipelineLayout pipelineLayout;
+    IPipelineBuilder(vk::Device device);
+    IPipelineBuilder(vk::Device device, vk::PipelineLayout layout);
 
-    std::vector<vk::DynamicState> dynamicStates {};
-    vk::PipelineDynamicStateCreateInfo dynamicState {};
-    std::vector<vk::VertexInputAttributeDescription> vertexAttributes;
-    std::vector<vk::VertexInputBindingDescription> vertexBindings;
-    vk::PipelineVertexInputStateCreateInfo vertexInputState {};
+    vk::ShaderModule loadShaderModule(const char* filename);
+
+    virtual vk::ResultValue<PipelineData> build() = 0;
+};
+
+class ComputePipelineBuilder : public IPipelineBuilder {
+public:
+    std::string m_functionName;
+    vk::ShaderModule m_shaderModule;
+
+    ComputePipelineBuilder(vk::Device device);
+    ComputePipelineBuilder(vk::Device device, vk::PipelineLayout layout);
+
+    ComputePipelineBuilder& setPipelineLayout(vk::PipelineLayout pipelineLayout);
+    ComputePipelineBuilder& setShaderModule(vk::ShaderModule shaderModule);
+    ComputePipelineBuilder& setSHaderModule(const char* filename);
+    ComputePipelineBuilder& setFunctionName(const char* functionName);
+
+    ComputePipelineBuilder& modify(std::function<void(ComputePipelineBuilder&)> func) { func(*this); return *this; }
+
+    vk::ResultValue<PipelineData> build() override;
+};
+
+class GraphicsPipelineBuilder : public IPipelineBuilder {
+public:
+    std::vector<vk::DynamicState> m_dynamicStates {};
+    vk::PipelineDynamicStateCreateInfo m_dynamicState {};
+    std::vector<vk::VertexInputAttributeDescription> m_vertexAttributes;
+    std::vector<vk::VertexInputBindingDescription> m_vertexBindings;
+    vk::PipelineVertexInputStateCreateInfo m_vertexInputState {};
     vk::PipelineInputAssemblyStateCreateInfo inputAssemblyState {};
-    std::optional<vk::PipelineDepthStencilStateCreateInfo> depthStencilState {};
-    std::optional<vk::PipelineTessellationStateCreateInfo> tessellationState {};
-    vk::PipelineRasterizationStateCreateInfo rasterizationState {};
-    vk::PipelineMultisampleStateCreateInfo multisampleState;
-    std::vector<vk::PipelineColorBlendAttachmentState> attachmentBlendStates;
-    vk::PipelineColorBlendStateCreateInfo colorBlendState {};
-    std::vector<vk::Format> colorAttachmentFormats {};
-    vk::PipelineRenderingCreateInfoKHR renderingCreateInfo {};
-    std::vector<vk::Rect2D> scissors {};
-    std::vector<vk::Viewport> viewports {};
-    vk::PipelineViewportStateCreateInfo viewportState {};
-    std::vector<vk::ShaderModule> modules;
-    std::vector<vk::PipelineShaderStageCreateInfo> stages {};
+    std::optional<vk::PipelineDepthStencilStateCreateInfo> m_depthStencilState {};
+    std::optional<vk::PipelineTessellationStateCreateInfo> m_tessellationState {};
+    vk::PipelineRasterizationStateCreateInfo m_rasterizationState {};
+    vk::PipelineMultisampleStateCreateInfo m_multisampleState;
+    std::vector<vk::PipelineColorBlendAttachmentState> m_attachmentBlendStates;
+    vk::PipelineColorBlendStateCreateInfo m_colorBlendState {};
+    std::vector<vk::Format> m_colorAttachmentFormats {};
+    vk::PipelineRenderingCreateInfoKHR m_renderingCreateInfo {};
+    std::vector<vk::Rect2D> m_scissors {};
+    std::vector<vk::Viewport> m_viewports {};
+    vk::PipelineViewportStateCreateInfo m_viewportState {};
+    std::vector<vk::PipelineShaderStageCreateInfo> m_stages {};
 
     GraphicsPipelineBuilder(vk::Device device);
     GraphicsPipelineBuilder(vk::Device device, vk::PipelineLayout);
@@ -70,17 +107,9 @@ public:
     GraphicsPipelineBuilder& setRenderingCreateInfo(vk::PipelineRenderingCreateInfoKHR rci);
     GraphicsPipelineBuilder& addColorAttachmentFormat(vk::Format format);
 
-    vk::ShaderModule loadShaderModule(const char* filename);
-
     GraphicsPipelineBuilder& modify(std::function<void(GraphicsPipelineBuilder&)> func) { func(*this); return *this; }
 
-    struct PipelineData {
-        std::vector<vk::ShaderModule> shaderModules;
-        vk::Pipeline pipeline;
-        vk::PipelineLayout pipelineLayout;
-    };
-
-    vk::ResultValue<PipelineData> build();
+    vk::ResultValue<PipelineData> build() override;
 };
 
 }
